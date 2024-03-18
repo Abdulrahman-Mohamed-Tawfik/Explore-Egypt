ce15951932403d594dbbeca6c9943210c409348c
Ahmedhatem307 <ahmedhatem.aez@gmail.com>
2024-03-18 15:19:21 +0200

    Filled 10 rows to the Landmark table

--- Explore_Egypt.DataAccess/Migrations/20240317154331_Added10rowsLandmark.Designer.cs
+++ Explore_Egypt.DataAccess/Migrations/20240317154331_Added10rowsLandmark.Designer.cs
@@ -4,6 +4,7 @@ using Explore_Egypt.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Explore_Egypt.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240317154331_Added10rowsLandmark")]
+    partial class Added10rowsLandmark
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
