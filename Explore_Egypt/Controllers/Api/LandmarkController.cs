430cb6ca3e9fdbb5e1e6ddbc4aab84af563ac22f
Abdelrahman Mohamed <abdelrahman.mohamed7337@gmail.com>
2024-06-21 17:54:01 +0300

    Add and Update Landmark controtller APIs - Add Api endpoint "Top10SearchLandmark" & "Top10FavoriteLandmark" - Add the record to search history at "GetLandmarkByName"

--- Explore_Egypt/Controllers/Api/LandmarkController.cs
+++ Explore_Egypt/Controllers/Api/LandmarkController.cs
@@ -38,41 +38,46 @@ namespace Explore_Egypt.Controllers.Api
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
-		public ActionResult<IEnumerable<Landmark>> GetLandmark([FromRoute(Name = "landmarkName")] string name, [FromBody] string userId)
-		{
-			if (name == null || userId == null || !_context.Users.Any(x => x.Id == userId))
-				return BadRequest();
+        public ActionResult<LandmarkDto> GetLandmark([FromRoute] string landmarkName, [FromBody] string userId)
+        {
+            if (userId == null || !_context.Users.Any(x => x.Id == userId))
+                return BadRequest();
 
-			var landmark = _context.Landmarks.FirstOrDefault(x => x.Name.Contains(name));
-			if (landmark == null)
-				return NotFound();
+            if (string.IsNullOrEmpty(landmarkName))
+                return BadRequest("Landmark name cannot be null or empty");
 
-			var landmarkDto = new LandmarkDto
-			{
-				Id = landmark.Id,
-				Name = landmark.Name,
-				Description = landmark.Description,
-				EgyptianStudentTicketPrice = landmark.EgyptianStudentTicketPrice,
-				EgyptianTicketPrice = landmark.EgyptianTicketPrice,
-				ForeignStudentTicketPrice = landmark.ForeignStudentTicketPrice,
-				ForeignTicketPrice = landmark.ForeignTicketPrice,
-				OpenTime = landmark.OpenTime,
-				CloseTime = landmark.CloseTime,
-				Latitude = landmark.Latitude,
-				Longitude = landmark.Longitude,
-				ImagesUrl = _context.LandmarkImages.Where(x => x.LandmarkId == landmark.Id).Select(x => x.Url).ToList()
-			};
-			_context.SearchHistory.Add(new SearchHistory
-			{
-				LandmarkID = landmark.Id,
-				UserId = userId,
-				Date = DateTime.Now
-			});
-			_context.SaveChanges();
-			return Ok(new { data = landmarkDto });
-		}
+            var landmark = _context.Landmarks.FirstOrDefault(x => x.Name.Contains(landmarkName));
+            if (landmark == null)
+                return NotFound("Landmark not found");
 
-		[HttpDelete]
+            var landmarkDto = new LandmarkDto
+            {
+                Id = landmark.Id,
+                Name = landmark.Name,
+                Description = landmark.Description,
+                EgyptianStudentTicketPrice = landmark.EgyptianStudentTicketPrice,
+                EgyptianTicketPrice = landmark.EgyptianTicketPrice,
+                ForeignStudentTicketPrice = landmark.ForeignStudentTicketPrice,
+                ForeignTicketPrice = landmark.ForeignTicketPrice,
+                OpenTime = landmark.OpenTime,
+                CloseTime = landmark.CloseTime,
+                Latitude = landmark.Latitude,
+                Longitude = landmark.Longitude,
+                ImagesUrl = _context.LandmarkImages.Where(x => x.LandmarkId == landmark.Id).Select(x => x.Url).ToList()
+            };
+            _context.SearchHistory.Add(new SearchHistory
+            {
+                LandmarkID = landmark.Id,
+                UserId = userId,
+                Date = DateTime.Now
+            });
+            _context.SaveChanges();
+            return Ok(new { data = landmarkDto });
+        }
+        
+
+
+        [HttpDelete]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -108,9 +113,41 @@ namespace Explore_Egypt.Controllers.Api
             return Ok(new { success = true, message = "Deleted Successfully" });
         }
 
-		
+		[HttpGet("Top10SearchLandmarks")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public ActionResult<IEnumerable<Landmark>> GetTop10SearchLandmarks()
+		{
+			if (_context.SearchHistory == null || _context.SearchHistory == null)
+				return BadRequest(new {msg = "no enough data"});
 
-		[HttpPost("predict", Name = "PreicateLandmarkNameFromImage")]
+			var landmarks =  _context.SearchHistory.GroupBy(sh => sh.LandmarkID)
+												.Select(g => new { LandmarkID = g.Key, Count = g.Count() })
+												.OrderByDescending(g => g.Count)
+												.Select(x => _context.Landmarks.FirstOrDefault(l => l.Id == x.LandmarkID))
+                                                .ToList();
+			return Ok(new { data = landmarks });
+
+        }
+
+        [HttpGet("Top10FavoriteLandmarks")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<IEnumerable<Landmark>> GetTop10FavoriteLandmarks()
+        {
+            if (_context.Favours == null || _context.SearchHistory == null)
+                return BadRequest(new { msg = "no enough data" });
+
+            var landmarks = _context.Favours.GroupBy(sh => sh.LandmarkID)
+                                          .Select(g => new { LandmarkID = g.Key, Count = g.Count() })
+                                          .OrderByDescending(g => g.Count)
+                                          .Select(x => _context.Landmarks.FirstOrDefault(l => l.Id == x.LandmarkID))
+                                          .ToList();
+            return Ok(new { data = landmarks });
+
+        }
+
+        [HttpPost("predict", Name = "PreicateLandmarkNameFromImage")]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		public async Task<ActionResult<string>> Predict([FromBody] ImageData imageData)
